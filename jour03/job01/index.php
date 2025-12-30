81d8b7e1fd509f989435adfd0d67a7f5ddc866c2
jawad zafari <jawad.zafari@laplateforme.io>
2025-12-30 15:53:12 +0100

    job01

---
+++ jour03/job01/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>j-query</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+    <h1>Job-01 jQuery</h1>
+    <button id="show-btn">Afficher le texte</button>
+
+    <button id="hide-btn">Cacher tout</button>
+    <hr>
+    <h2 id="container" ></h2>
+
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ jour03/job01/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#show-btn").click(function(){
+        $("#container").text("Les logiciels et les cathédrales, c'est un peu la même chose d'abord on les construit, ensuite on prie.");
+    });
+
+
+    $("#hide-btn").click(function(){
+    $("html").hide();
+});
+
+});
+
+
