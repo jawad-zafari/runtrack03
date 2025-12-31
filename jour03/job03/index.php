b2d82acc0bec97810bc03dc32f91c73038593fd0
jawad zafari <jawad.zafari@laplateforme.io>
2025-12-31 13:30:56 +0100

    first

---
+++ jour03/job03/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Taquin</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>le puzzle coulissant</h1>
+    <div id="game-board"></div>
+    <p id="message"></p>
+    <button id="restart-btn">Recommencer</button>
+    <script src="script.js"></script>
+</body>
+</html>
